a9176556df661b06f80b3787ae61b4f8d3f16d0d
shantanu-sigmoid <shantanut@sigmoidanalytics.com>
2022-03-21 14:40:10 +0530

    upsert problem in dag_run resolved

--- dags/sql/create_new_table.sql
+++ dags/sql/create_new_table.sql
@@ -1,2 +1,10 @@
-CREATE TABLE {{params.table_name}} AS
-SELECT * FROM dag_run;
+CREATE TABLE IF NOT EXISTS {{params.table_name}} (
+    id INT PRIMARY KEY,
+    dag_id VARCHAR NOT NULL,
+    execution_date VARCHAR NOT NULL
+);
+INSERT INTO {{params.table_name}}(id, dag_id, execution_date)
+SELECT id, dag_id, execution_date from dag_run
+ON CONFLICT (id)
+DO NOTHING;
+
