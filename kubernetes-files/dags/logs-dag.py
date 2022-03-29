04a2823d17ab62683da96a805ef1d7bc7949dca9
shantanu-sigmoid <shantanut@sigmoidanalytics.com>
2022-03-29 14:43:15 +0530

    working dag

--- kubernetes-files/dags/logs-dag.py
+++ kubernetes-files/dags/logs-dag.py
@@ -20,29 +20,32 @@ default_args = {
 }
 
 query = """
-CREATE TABLE IF NOT EXISTS dag_run_copy (
-    id INT PRIMARY KEY,
-    dag_id VARCHAR NOT NULL,
-    execution_date VARCHAR NOT NULL
-);
-INSERT INTO dag_run_copy(id, dag_id, execution_date)
-SELECT id, dag_id, execution_date from dag_run
-ON CONFLICT (id)
-DO NOTHING;
-"""
+        CREATE TABLE IF NOT EXISTS dag_run_copy (
+            id INT PRIMARY KEY,
+            dag_id VARCHAR NOT NULL,
+            execution_date VARCHAR NOT NULL
+        );
+        INSERT INTO dag_run_copy(id, dag_id, execution_date)
+        SELECT id, dag_id, execution_date from dag_run
+        ON CONFLICT (id)
+        DO NOTHING;
+        SELECT * FROM dag_run_copy;
+        """
 
 def execute_query_with_psycopg():
     # postgresql://airflow:airflow@postgres-service-db:5432/airflow
+    print("Connection to airflow database established")
     conn = psycopg2.connect(host="postgres-service-db", database = "airflow", user = "airflow", password = "airflow", port = "5432")
     cur = conn.cursor()
+    print("Running query...")
     cur.execute(query)
+    print("Query data...")
+    print(cur.fetchall())
+
 
 
 with DAG("time_of_dag_execution", default_args=default_args, schedule_interval="0 6 * * *", catchup = False) as dag:
 
     t1 = DummyOperator(task_id="start_task")
-
-    # t2 = PostgresOperator(task_id="create_new_table_in_postgre", postgres_conn_id='postgres_conn', \
-    #     sql="sql/create_new_table.sql", params = {"table_name": "time_of_dag_exec"})
-    t3 = PythonOperator(task_id="create_table", python_callable = execute_query_with_psycopg)
-    t1 >> t3
+    t2 = PythonOperator(task_id="create_table", python_callable = execute_query_with_psycopg)
+    t1 >> t2
